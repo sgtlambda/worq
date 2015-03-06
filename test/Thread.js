e0918a9c96671adacf3f63c7dc323e3566ef3959
jmversteeg <janmerijnversteeg@gmail.com>
2015-03-06 18:33:57 +0100

    Add tests for lib/Thread.js

---
+++ test/Thread.js
@@ -0,0 +1,23 @@
+var should = require('should'),
+    Job    = require('../lib/Job'),
+    Thread = require('../lib/Thread');
+
+describe('Thread', function () {
+
+    var thread = new Thread();
+
+    it('should have a job property', function () {
+        thread.should.have.property('job');
+    });
+
+    describe('#isOpen', function () {
+
+        it('should return false if the job property is not null', function () {
+            thread.isOpen().should.be.true;
+            thread.job = new Job();
+            thread.isOpen().should.be.false;
+        });
+
+    });
+
+});
