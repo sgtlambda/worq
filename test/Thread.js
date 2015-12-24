d12b6c084b4851dc7759cf9a91a2709a23cd1768
JM Versteeg <janmerijnversteeg@gmail.com>
2015-12-24 19:41:01 +0100

    Use pinkie-promise

--- test/Thread.js
+++ test/Thread.js
@@ -12,13 +12,13 @@ describe('Thread', () => {
 
     describe('run()', () => {
         it('should return a promise for the completion of the job', () => {
-            return thread.enqueue(new Job(() => Promise.delay(10))).should.be.fulfilled;
+            return thread.run(new Job(() => Promise.delay(10))).should.be.fulfilled;
         });
     });
 
     describe('isPending()', () => {
         it('should return true if the job has not fulfilled yet', () => {
-            thread.enqueue(new Job(() => Promise.delay(10)));
+            thread.run(new Job(() => Promise.delay(10)));
             thread.isPending().should.be.true;
             return Promise.delay(20).then(() => {
                 thread.isPending().should.be.false;
